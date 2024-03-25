989a88b51d3f262ec0480b6cf7819b76b2324fee
Georges034302 <georges.boughantous@gmail.com>
2024-03-25 19:35:14 +1100

    wslab6

--- js/math.js
+++ js/math.js
@@ -1,11 +1,12 @@
-var factorial = function(num) {
+function factorial(num){
     let f = 1;
-    for (let i = 1; i <= num; i++)
-        f *= i;
+    for(let i=2;i<=num;i++){
+        f*=i //f = f * i
+    }
     return f;
 }
 
-var calculate = function() {
-    var num = document.getElementById("num").value;
-    document.getElementById("displayDiv").innerHTML = "Factorial(" + num + ") = " + factorial(num);
+var calculate = function(){
+    var num = document.getElementById('num').value;
+    document.getElementById('output_div').innerHTML = "Factorial("+num+") = "+factorial(num);
 }
